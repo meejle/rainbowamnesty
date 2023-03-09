4d4ae8e9cc213b694fb5f042251d253f4222fc36
meejle <inbox@mikelee.me.uk>
2023-03-09 02:13:39 +0000

    Reworked field name boxes to fit way more characters (as unobtrusively as possible).

--- src/field_name_box.c
+++ src/field_name_box.c
@@ -7,7 +7,7 @@
 #include "text.h"
 #include "field_name_box.h"
 
-#define NAMEBOX_TAG 0x2722
+#define NAMEBOX_TAG 0x4E44
 
 //  Usage:
 //  namebox <text>  ->  displays the namebox with the given text
@@ -18,11 +18,13 @@ static void LoadNameboxTilemap();
 static void CreateTask_DisplayNamebox();
 static void Task_DisplayNamebox(u8 taskId);
 static void LoadNameboxSprite(s8 x, s8 y);
+static void LoadNameboxSprite2(s8 x, s8 y);
 static void AddTextPrinterForName();
 static void ClearNameboxTiles();
 
 static EWRAM_DATA u8 sNameboxWindowId = 0;
 static EWRAM_DATA u8 sNameboxGfxId = 0;
+static EWRAM_DATA u8 sNameboxGfxId2 = 0;
 
 static const u32 sNamebox_Gfx[] = INCBIN_U32("graphics/text_window/name_box.4bpp.lz");
 static const u16 sNamebox_Pal[] = INCBIN_U16("graphics/text_window/name_box.gbapal");
@@ -36,7 +38,21 @@ static const struct OamData sOam_Namebox =
     .bpp = ST_OAM_4BPP,
     .shape = SPRITE_SHAPE(64x32),
     .size = SPRITE_SIZE(64x32),
-    .tileNum = 0,
+    .tileNum = 1,
+    .priority = 1,
+    .paletteNum = 0,
+};
+
+static const struct OamData sOam_Namebox2 =
+{
+    .x = 0,
+    .y = 0,
+    .affineMode = ST_OAM_AFFINE_OFF,
+    .objMode = ST_OAM_OBJ_NORMAL,
+    .bpp = ST_OAM_4BPP,
+    .shape = SPRITE_SHAPE(64x32),
+    .size = SPRITE_SIZE(64x32),
+    .tileNum = 1,
     .priority = 1,
     .paletteNum = 0,
 };
@@ -44,7 +60,7 @@ static const struct OamData sOam_Namebox =
 
 static const struct CompressedSpriteSheet sSpriteSheet_Namebox = {
     .data = sNamebox_Gfx,
-    .size = 1024,
+    .size = 2048,
     .tag = NAMEBOX_TAG,
 };
 
@@ -53,8 +69,17 @@ static const union AnimCmd sSpriteAnim_Namebox[] = {
     ANIMCMD_END
 };
 
+static const union AnimCmd sSpriteAnim_Namebox2[] = {
+    ANIMCMD_FRAME(32, 0),
+    ANIMCMD_END
+};
+
 static const union AnimCmd *const sSpriteAnimTable_Namebox[] = {
     sSpriteAnim_Namebox,
+};
+
+static const union AnimCmd *const sSpriteAnimTable_Namebox2[] = {
+    sSpriteAnim_Namebox2,
 };
 
 static const struct SpriteTemplate sSpriteTemplate_Namebox = {
@@ -62,6 +87,16 @@ static const struct SpriteTemplate sSpriteTemplate_Namebox = {
     .paletteTag = NAMEBOX_TAG,
     .oam = &sOam_Namebox,
     .anims = sSpriteAnimTable_Namebox,
+    .images = NULL,
+    .affineAnims = gDummySpriteAffineAnimTable,
+    .callback = SpriteCallbackDummy
+};
+
+static const struct SpriteTemplate sSpriteTemplate_Namebox2 = {
+    .tileTag = NAMEBOX_TAG,
+    .paletteTag = NAMEBOX_TAG,
+    .oam = &sOam_Namebox2,
+    .anims = sSpriteAnimTable_Namebox2,
     .images = NULL,
     .affineAnims = gDummySpriteAffineAnimTable,
     .callback = SpriteCallbackDummy
@@ -75,12 +110,12 @@ static const struct SpritePalette sSpritePalette_Namebox = {
 static const struct WindowTemplate sNamebox_WindowTemplate =
 {
     .bg = 0,
-    .tilemapLeft = 1, 
+    .tilemapLeft = 0, 
     .tilemapTop = 12,
-    .width = 6, 
+    .width = 16, 
     .height = 2,                //required to be at least two
     .paletteNum = 15,
-    .baseBlock = 0x188          //placed before the msgbox tiles
+    .baseBlock = 0x174          //placed before the msgbox tiles
 };
 
 
@@ -105,6 +140,7 @@ void ClearNamebox() {
     RemoveWindow(sNameboxWindowId);
     sNameboxWindowId = 0;
     DestroySpriteAndFreeResources(&gSprites[sNameboxGfxId]);
+    DestroySpriteAndFreeResources(&gSprites[sNameboxGfxId2]);
 }
 
 
@@ -122,7 +158,8 @@ static void Task_DisplayNamebox(u8 taskId) {
         gTasks[taskId].tTimer--;
     else{
         LoadNameboxTilemap();
-        LoadNameboxSprite(32, 104);
+        LoadNameboxSprite(32, 111);
+        LoadNameboxSprite2(96, 111);
         
         DestroyTask(taskId);
     }
@@ -139,18 +176,28 @@ static void LoadNameboxWindow(const struct WindowTemplate *window) {
 
 static void LoadNameboxTilemap() {
     PutWindowTilemap(sNameboxWindowId);
+    LoadCompressedSpriteSheet(&sSpriteSheet_Namebox);
+    LoadSpritePalette(&sSpritePalette_Namebox);
 }
 
 static void LoadNameboxSprite(s8 x, s8 y) {
     u8 spriteId;
-    
-    LoadCompressedSpriteSheet(&sSpriteSheet_Namebox);
-    LoadSpritePalette(&sSpritePalette_Namebox);
+
     spriteId = CreateSprite(&sSpriteTemplate_Namebox, x, y, 0);
     if (sNameboxGfxId == MAX_SPRITES)
         return;
     else
         sNameboxGfxId = spriteId;
+}
+
+static void LoadNameboxSprite2(s8 x, s8 y) {
+    u8 spriteId2;
+
+    spriteId2 = CreateSprite(&sSpriteTemplate_Namebox2, x, y, 0);
+    if (sNameboxGfxId2 == MAX_SPRITES)
+        return;
+    else
+        sNameboxGfxId2 = spriteId2;
 }
 
 static void AddTextPrinterForName() {
@@ -159,13 +206,13 @@ static void AddTextPrinterForName() {
     printer.currentChar = gStringVar3;
     printer.windowId = sNameboxWindowId;
     printer.fontId = FONT_SMALL_SUBPIXEL; // Can use FONT_NARROW, etc, here.
-    printer.x = 5; 
-    printer.y = 0;
+    printer.x = 7; 
+    printer.y = 4;
     printer.currentX = printer.x;
     printer.currentY = printer.y;
-    printer.fgColor = GetFontAttribute(1, FONTATTR_COLOR_FOREGROUND);
+    printer.fgColor = TEXT_COLOR_WHITE;
     printer.bgColor = TEXT_COLOR_TRANSPARENT;
-    printer.shadowColor = GetFontAttribute(1, FONTATTR_COLOR_SHADOW);
+    printer.shadowColor = TEXT_COLOR_TRANSPARENT;
     printer.unk = GetFontAttribute(1, FONTATTR_UNKNOWN);
     printer.letterSpacing = 0;
     printer.lineSpacing = 0;
